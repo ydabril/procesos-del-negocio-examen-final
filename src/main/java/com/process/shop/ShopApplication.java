a29b5a865c951b82337601e71ca93d607a58f5b9
Jerson Garcia <jersonjavier@gmail.com>
2024-02-29 21:12:49 -0500

    init commit

---
+++ src/main/java/com/process/shop/ShopApplication.java
@@ -0,0 +1,24 @@
+package com.process.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public static String helloWorld(){
+		System.out.println("Hello World!");
+		return "Hello World!";
+	}
+
+}
