15c08afbf6d3d952676e580db6f599e7581ebd3a
Jerson Garcia <jersonjavier@gmail.com>
2024-03-21 21:14:10 -0500

    persistencia

--- src/main/java/com/process/shop/controller/UserController.java
+++ src/main/java/com/process/shop/controller/UserController.java
@@ -3,16 +3,20 @@ package com.process.shop.controller;
 import com.process.shop.model.User;
 import com.process.shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/v1/user")
 public class UserController {
     @Autowired
     private UserService userService;
-    @GetMapping("user/{id}")
-    public User getUserById(){
-        return userService.getUserById(1L);
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@RequestParam Long id){
+        return userService.getUserById(id);
     }
 }
 
--- src/main/java/com/process/shop/service/UserServiceImpl.java
+++ src/main/java/com/process/shop/service/UserServiceImpl.java
@@ -3,44 +3,46 @@ package com.process.shop.service;
 import com.process.shop.model.Address;
 import com.process.shop.model.User;
 import com.process.shop.model.enums.DocumentType;
+import com.process.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User createUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(User userUpdated, Long id) {
-        return null;
+        Optional<User> userBd = userRepository.findById(id);
+        if(userBd.isEmpty()){
+            return null;
+        }
+        userBd.get().setFullName(userUpdated.getFullName());
+        userBd.get().setPhoneNumber(userUpdated.getPhoneNumber());
+        return userRepository.save(userBd.get());
     }
 
     @Override
     public User getUserById(Long id) {
-        return User.builder()
-                .fullName("Pepito perez")
-                .document("123")
-                .documentType(DocumentType.CC)
-                .address(List.of(
-                        Address.builder()
-                        .avenue("1212")
-                        .neighborhood("12131")
-                        .build(),
-                        Address.builder()
-                                .avenue("43434343")
-                                .neighborhood("DSDDSDSDSD")
-                                .build()
-                        )
-                )
-                .build();
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            return null;
+        }
+        return user.get();
     }
 
     @Override
     public List<User> findAllUsers() {
-        return null;
+        return (List<User>) userRepository.findAll();
     }
 }
