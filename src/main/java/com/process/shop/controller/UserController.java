cd56ec7d9ef9260029c79c08d363dccd108837fc
Jerson garcia <jersonjavier@gmail.com>
2024-04-04 19:46:53 -0500

    Relacion usuario y direccion

--- src/main/java/com/process/shop/controller/UserController.java
+++ src/main/java/com/process/shop/controller/UserController.java
@@ -5,6 +5,8 @@ import com.process.shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/user")
 public class UserController {
@@ -15,8 +17,16 @@ public class UserController {
         return userService.createUser(user);
     }
     @GetMapping("/{id}")
-    public User getUserById(@RequestParam Long id){
+    public User getUserById(@PathVariable Long id){
         return userService.getUserById(id);
+    }
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id){
+        return userService.updateUser(user, id);
+    }
+    @GetMapping
+    public List<User> allUsers(){
+        return userService.findAllUsers();
     }
 }
 
--- src/main/java/com/process/shop/model/User.java
+++ src/main/java/com/process/shop/model/User.java
@@ -1,15 +1,11 @@
 package com.process.shop.model;
-import com.process.shop.model.enums.DocumentType;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
-
 @Data
-@Builder
+@NoArgsConstructor
 @Entity
 public class User {
     @Id
@@ -17,10 +13,11 @@ public class User {
     private Long id;
     private String fullName;
     private String document;
-    private DocumentType documentType;
+    private String documentType;
     private LocalDate birthDay;
     private String phoneNumber;
     private String email;
     private String password;
-    //private List<Address> address;
+    @OneToMany(mappedBy = "user")
+    private List<Address> address;
 }
