f9e2471c317cd5db7e217e112248cba9dfe0f224
Jerson Garcia <jersonjavier@gmail.com>
2024-05-09 20:41:53 -0500

    Validator spring

--- src/main/java/com/process/shop/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/process/shop/exceptions/CustomExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.process.shop.exceptions;
 
 import com.process.shop.model.dto.Response;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
@@ -36,4 +42,44 @@ public class CustomExceptionHandler {
                 HttpStatus.CONFLICT
         );
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response> handleValidationExceptions(
+            MethodArgumentNotValidException ex){
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.add(
+                    ((FieldError) error).getField().concat(
+                            " - "+error.getDefaultMessage()
+                    )
+            );
+            }
+        );
+        return new ResponseEntity<>(
+                Response.builder()
+                        .date(LocalDate.now())
+                        .message(errors)
+                        .statusCode(HttpStatus.BAD_REQUEST.name())
+                        .build(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<Response> handleConstraintViolationException(MethodArgumentTypeMismatchException e) {
+        return new ResponseEntity<>(
+                Response.builder()
+                        .date(LocalDate.now())
+                        .message(List.of("the value must be an integer"))
+                        .statusCode(HttpStatus.BAD_REQUEST.name())
+                        .build(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
 }
+
+
+
+
+
+
+
